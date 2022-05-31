bb9730c0def324b8df35d0ea2510e5449adea486
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-05-31 23:13:14 +0300

    run all

--- build_sshash.py
+++ build_sshash.py
@@ -8,6 +8,6 @@ for D in datasets:
     f = datasets[D]
     logfile = index_dir + "/" + D + ".sshash.log"
     run("/usr/bin/time --verbose ./sshash/build/build {} {} {} -o {} 2>&1 | tee {}".format(
-        unitig_dir + "/" + D + ".unitigs.fa", k, minimizer_len, index_dir + "/" + D + ".sshash", logfile))
+        unitig_dir + "/" + D + ".unitigs.fa.ust.fa", k, minimizer_len, index_dir + "/" + D + ".sshash", logfile))
 
 
---
+++ run_all.bash
@@ -0,0 +1,12 @@
+set -xue
+
+SIZE="--small"
+#SIZE="--large"
+
+python3 build_unitigs.py $SIZE # requires bcalm or cuttlefish
+python3 stitch_unitigs.py $SIZE # requires ust
+python3 build_sbwt.py $SIZE # requires sbwt
+python3 build_bifrost.py $SIZE # requires bifrost with a modification that prints query time
+python3 build_sshash.py $SIZE # requires sshash
+python3 generate_all_queries.py $SIZE # requires running compile.sh
+python3 query_sbwt.py $SIZE # requires running compile.sh

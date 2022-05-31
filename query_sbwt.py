350e0df49e90901916f0153f287efe1885b0828e
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-05-31 16:19:06 +0300

    query sbwt

---
+++ query_sbwt.py
@@ -0,0 +1,23 @@
+from setup import *
+
+print(datasets)  # Defined in setup.py
+
+variants = ["rrr-matrix", "mef-matrix", "plain-split", "rrr-split","mef-split", "plain-concat", "mef-concat", "plain-subsetwt", "rrr-subsetwt"]
+
+for D in datasets:
+    f = datasets[D]
+
+    for variant in variants:
+        # Positive queries
+        run("/usr/bin/time --verbose ./SBWT/build/bin/sbwt search -o {} -i {} -q {} 2>&1 | tee {}".format(
+            query_dir + "/" + D + ".pos." + variant + ".sbwt.out",
+            index_dir + "/" + D + "." + variant + ".sbwt",
+            query_dir + "/" + D + ".pos.fna",
+            query_dir + "/" + D + ".pos." + variant + ".sbwt.log",))
+
+        # Negative queries
+        run("/usr/bin/time --verbose ./SBWT/build/bin/sbwt search -o {} -i {} -q {} 2>&1 | tee {}".format(
+            query_dir + "/" + D + ".neg." + variant + ".sbwt.out",
+            index_dir + "/" + D + "." + variant + ".sbwt",
+            query_dir + "/" + D + ".neg.fna",
+            query_dir + "/" + D + ".neg." + variant + ".sbwt.log",))

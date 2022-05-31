e63202bd181d3590af0f46169f66a7ea2421182e
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-05-31 16:03:26 +0300

    notes

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 set -xue
 
-python3 build_unitigs.py --small
-python3 stitch_unitigs.py --small
-python3 build_sbwt.py --small
-python3 build_sshash.py --small
-python3 generate_all_queries.py --small
+python3 build_unitigs.py --small # requires bcalm and cuttlefish
+python3 stitch_unitigs.py --small # requires ust
+python3 build_sbwt.py --small # requires sbwt
+python3 build_sshash.py --small # requires sshash
+python3 generate_all_queries.py --small # requires running compile.sh

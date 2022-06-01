fcb16068684e1bc012b26cb449c93c235136a861
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-06-02 02:07:01 +0300

    Turns out VARI automatically adds reverse complements during sorting?

--- build_VARI.py
+++ build_VARI.py
@@ -10,18 +10,10 @@ from setup import *
 for D in datasets:
     f = datasets[D]
 
-    # Create reverse complement file (not timed)
-    rc_file_name = temp_dir + "/" + D + "_rc" + (".fq" if D == "metagenome" else ".fna")
-    run("./SeqIO/rc_file {} {}".format(f, rc_file_name))
-
-    # Create input list for KMC
-    run("echo {} > {}".format(f, temp_dir + "/kmc_input_list.txt"))
-    run("echo {} >> {}".format(rc_file_name, temp_dir + "/kmc_input_list.txt"))
-
-    run("/usr/bin/time --verbose ./KMC/bin/kmc -b -ci0 {} -k{} @{} {} {}  2>&1 | tee {}".format(
+    run("/usr/bin/time --verbose ./KMC/bin/kmc -ci0 {} -k{} {} {} {}  2>&1 | tee {}".format(
           "-fq" if D == "metagenome" else "-fm", # Metagenome is in fastq format, others in multi-fasta
           k, 
-          temp_dir + "/kmc_input_list.txt",
+          f,
           temp_dir + "/kmc1",
           temp_dir,
           index_dir + "/" + D + ".kmc1.log"
@@ -44,4 +36,3 @@ for D in datasets:
         index_dir + "/" + D + ".vari.log"
     ))
 
-    run("rm " + rc_file_name) # Clean up

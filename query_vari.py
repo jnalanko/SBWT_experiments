ccfa4781f80c31ae8a5789b684677a959ebb9f62
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-06-02 18:49:11 +0300

    Fix

--- query_vari.py
+++ query_vari.py
@@ -10,19 +10,19 @@ for D in datasets:
         query_dir + "/" + D + ".pos.vari.log",))
 
     # Negative queries
-    run("/usr/bin/time --verbose ./sshash/build/query {} {} 2>&1 | tee {}".format(
+    run("/usr/bin/time --verbose ./cosmo/cosmo-benchmark {} {} 2>&1 | tee {}".format(
         index_dir + "/" + D + ".vari_list.dbg",
         query_dir + "/" + D + ".neg.fa",
         query_dir + "/" + D + ".neg.vari.log",))
 
     # Streaming positive queries
-    run("/usr/bin/time --verbose ./sshash/build/query {} {} 2>&1 | tee {}".format(
+    run("/usr/bin/time --verbose ./cosmo/cosmo-benchmark {} {} 2>&1 | tee {}".format(
         index_dir + "/" + D + ".vari_list.dbg",
         query_dir + "/" + D + ".streaming.pos.fa",
         query_dir + "/" + D + ".streaming.pos.vari.log",))
 
     # Steaming negative queries
-    run("/usr/bin/time --verbose ./sshash/build/query {} {} 2>&1 | tee {}".format(
+    run("/usr/bin/time --verbose ./cosmo/cosmo-benchmark {} {} 2>&1 | tee {}".format(
         index_dir + "/" + D + ".vari_list.dbg",
         query_dir + "/" + D + ".streaming.neg.fa",
         query_dir + "/" + D + ".streaming.neg.vari.log",))

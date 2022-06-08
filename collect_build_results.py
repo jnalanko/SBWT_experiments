6c478fa2ef2e522b8b9175d70533676e4b1cfbc7
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-06-08 23:25:10 +0300

    Collect build

--- collect_build_results.py
+++ collect_build_results.py
@@ -1,7 +1,39 @@
 from setup import *
 from collections import defaultdict
 
+def parse_kmer_count(logfile):
+    for line in open(logfile):
+        if "Build SBWT for" in line and "distinct k-mers" in line:
+            return int(line.split()[-3])
+    assert(False) # Should not come here
+
+def parse_subset_count(logfile):
+    for line in open(logfile):
+        if "SBWT has " in line and "subsets" in line:
+            return int(line.split()[-2])
+    assert(False) # Should not come here
+
+def print_table(dataset):
+    streaming_overhead_rc = parse_subset_count(index_dir + "/" + dataset + ".plain-matrix.log")
+    streaming_overhead_no_rc = parse_subset_count(index_dir + "/" + dataset + ".plain-matrix-no-rc.log")
+    kmers = parse_kmer_count(index_dir + "/" + dataset + ".plain-matrix-no-rc.log")
+
+    for variant in variants:
+        size_rc = (sizes[variant][dataset]*8 - streaming_overhead_rc) / kmers
+        size_no_rc = (sizes[variant + "-no-rc"][dataset]*8 - streaming_overhead_no_rc) / kmers
+        print("& " + variant, 
+            "& {} | {}".format(int(times[variant][dataset]), int(times[variant + "-no-rc"][dataset])),  # Seconds
+            "& {:.3g} | {:.3g}".format(mems[variant][dataset] / 2**30, mems[variant + "-no-rc"][dataset] / 2**30), # GB
+            "& {:.3g} | {:.3g}".format(size_rc, size_no_rc)) # bits / kmer
+
+    for variant in ["sshash", "bifrost"]: 
+        print("& " + variant, 
+            "& {} ".format(int(times[variant][dataset])),  # Seconds
+            "& {:.3g} ".format(mems[variant][dataset] / 2**30), # GB
+            "& {:.3g} ".format(sizes[variant][dataset]*8 / kmers)) # bits / kmer
+
 variants = ["plain-matrix", "rrr-matrix", "mef-matrix", "plain-split", "rrr-split","mef-split", "plain-concat", "mef-concat", "plain-subsetwt", "rrr-subsetwt"]
+variants_both = variants + [x + "-no-rc" for x in variants] # Add no-reverse-complement versions
 
 times = defaultdict(lambda: defaultdict(float)) # tool -> dataset -> time in seconds
 mems = defaultdict(lambda: defaultdict(int)) # tool -> dataset -> peak RSS in bytes
@@ -10,13 +42,16 @@ sizes = defaultdict(lambda: defaultdict(int)) # tool -> dataset -> final size on
 # Get times and memories from /usr/bin/time --verbose output
 for D in datasets:
     # SBWT variants
-    for variant in variants:
+    for variant in variants_both:
         logfile = index_dir + "/" + D + "." + variant + ".log"
         time, rss = parse_usr_bin_time(logfile)
 
-        if variant != "plain-matrix":
+        if "plain-matrix" not in variant:
             # Since we are building through plain matrix, we need to include the time and memory for that
-            time_plain, rss_plain = parse_usr_bin_time(index_dir + "/" + D + ".plain-matrix.log")
+            if variant[-5:] == "no-rc":
+                time_plain, rss_plain = parse_usr_bin_time(index_dir + "/" + D + ".plain-matrix-no-rc.log")
+            else:
+                time_plain, rss_plain = parse_usr_bin_time(index_dir + "/" + D + ".plain-matrix.log")
             rss = max(rss, rss_plain)
             time += time_plain 
 
@@ -42,29 +77,27 @@ for D in datasets:
 # Get sizes on disk
 ls = run_get_output("ls -l index")
 for line in ls.split("\n")[1:]:
-
     if line.split()[-1].split(".")[-1] in ["sbwt", "sshash", "gfa"]:
         size = line.split()[4]
         filename = line.split()[-1]
         dataset = filename.split(".")[0]
         method = filename.split(".")[1]
-        sizes[method][dataset] = size
+        sizes[method][dataset] = int(size)
 
 
 print("")
 print("Covid")
-for variant in (variants + ["sshash", "bifrost"]):
-    print(variant, times[variant]["covid"], mems[variant]["covid"], sizes[variant]["covid"])
+print_table("covid")
 
 print("")
 print("Ecoli")
-for variant in (variants + ["sshash", "bifrost"]):
-    print(variant, times[variant]["ecoli"], mems[variant]["ecoli"], sizes[variant]["ecoli"])
+print_table("ecoli")
+
 
 print("")
-print("Metagenome")
-for variant in (variants + ["sshash", "bifrost"]):
-    print(variant, times[variant]["metagenome"], mems[variant]["metagenome"], sizes[variant]["metagenome"])
+print("metagenome")
+print_table("metagenome")
+
 
 
         
